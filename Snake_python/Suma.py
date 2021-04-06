2dae03d82f9591b80f772074151bab6c20e1955c
SebastianDiaz-GIT <70162961+SebastianDiaz-GIT@users.noreply.github.com>
2021-04-05 19:31:48 -0500

    vr1.1
    Aprendiendo a usar VIM

---
+++ Snake_python/Suma.py
@@ -0,0 +1,10 @@
+print("Bienvenido a Suma basica con python")
+print("Dijita un numero entero")
+    #input() equivale a Scanner
+num1 = int(input())
+print("dijita otro numero que deseas sumar")
+num2 = int(input())
+#total
+Total = num1 + num2
+print(f"El resultado total de la Suma es: {Total}")
+
